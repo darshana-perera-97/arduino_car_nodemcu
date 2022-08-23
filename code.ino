8c637306d083f26ade68792235293f02ed64ee60
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-23 21:33:00 +0530

    declearing variables

--- code.ino
+++ code.ino
@@ -1,2 +1,17 @@
 #include <ESP8266WiFi.h>
 
+const char* ssid     = "Dialog_4G";
+const char* password = "darshana$123";
+
+WiFiServer server(80);
+String header;
+
+String output4State = "off";
+
+const int output4 = 4;
+
+unsigned long currentTime = millis();
+unsigned long previousTime = 0; 
+const long timeoutTime = 2000;
+
+
