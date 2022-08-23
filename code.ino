3dd90274aac7f9056a1bf6094224612ac7944546
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-23 21:34:26 +0530

    setup() created

--- code.ino
+++ code.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "Dialog_4G";
-const char* password = "darshana$123";
+const char *ssid = "Dialog_4G";
+const char *password = "darshana$123";
 
 WiFiServer server(80);
 String header;
@@ -11,7 +11,25 @@ String output4State = "off";
 const int output4 = 4;
 
 unsigned long currentTime = millis();
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
-
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(output4, OUTPUT);
+    digitalWrite(output4, LOW);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
+}
